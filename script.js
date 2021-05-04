c324434d985fd6fc7c9bb6c366b45b03076fb5d6
Vishal Pillai <68294182+VishalPillai-2020@users.noreply.github.com>
2021-05-04 19:38:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function showhelp(){
+    
+    p_1 =  document.getElementById("p_1");
+    p_1.innerHTML="<span>To play the song Sorry get your left wrist <br>in front of the webcam and to play the song<br> Darkside get your right wrist in front of the webcam </span>";
+}
+function disablehelp(){
+    p_1.innerHTML="Help";
+}
