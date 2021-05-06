d0f52ebad929ee17475bde5f2459c1d4dcaa4613
Vishal Pillai <68294182+VishalPillai-2020@users.noreply.github.com>
2021-05-06 14:05:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-
+song_1="";
+song_2="";
+song_name_element=document.getElementById("song_name");
+function preload(){
+    song_1=loadSound("DarkSide.mp3");
+    song_2=loadSound("Sorry.mp3")
+}
 function showhelp(){
     
     p_1 =  document.getElementById("p_1");
@@ -7,3 +13,26 @@ function showhelp(){
 function disablehelp(){
     p_1.innerHTML="Help";
 }
+function setup(){
+    video=createCapture(VIDEO);
+    video.hide();
+    canvas=createCanvas(500,350);
+    canvas.center();
+}
+function draw(){
+     image(video,0,0,500,400)
+}
+function play_1(){
+    song_name_element.innerHTML="The Current Playing Song Is";
+    song_2.stop();
+    song_1.stop();
+song_2.play();
+song_name_element.innerHTML+=" Sorry";
+}
+function play(){
+    song_name_element.innerHTML="The Current Playing Song Is";
+    song_1.stop();
+    song_2.stop();
+    song_1.play();
+    song_name_element.innerHTML+=" DarkSide";
+    }
