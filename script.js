434a21e260c9fc31a7fa5d0440fd8dfc5c0cab42
Vishal Pillai <68294182+VishalPillai-2020@users.noreply.github.com>
2021-05-18 11:40:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,14 @@ rightwristx = "";
 rightwristy = "";
 leftwristx = "";
 leftwristy = "";
+score_leftWrist = "";
+score_rightWrist = "";
 song_name_element = document.getElementById("song_name");
+var SpeechRecognition = window.webkitSpeechRecognition;
 
+var recognition = new SpeechRecognition();
+recognition.continuous = true;
+recognition.interimResults = true;
 function preload() {
     song_1 = loadSound("DarkSide.mp3");
     song_2 = loadSound("Sorry.mp3")
@@ -42,13 +48,30 @@ function gotPoses(results) {
         rightwristx = results[0].pose.rightWrist.x;
         rightwristy = results[0].pose.rightWrist.y;
         leftwristx = results[0].pose.leftWrist.x;
-        leftwristy= results[0].pose.leftWrist.y;
-        console.log("Right Wrist X = "+rightwristx+" Right Wrist Y = "+rightwristy+" Left Wrist X = "+leftwristx+" Left Wrist Y = "+leftwristy);
+        leftwristy = results[0].pose.leftWrist.y;
+        score_leftWrist = results[0].pose.keypoints[9].score;
+        score_rightWrist = results[0].pose.keypoints[10].score;
+        console.log("Score of Left Wrist = " + score_leftWrist + " Score of right wrist = " + score_rightWrist);
+        console.log("Right Wrist X = " + rightwristx + " Right Wrist Y = " + rightwristy + " Left Wrist X = " + leftwristx + " Left Wrist Y = " + leftwristy);
+
     }
 }
 
 function draw() {
-    image(video, 0, 0, 500, 400)
+    image(video, 0, 0, 500, 400);
+    if (score_leftWrist > 0.2 && !song_2.isPlaying()) {
+        song_1.stop();
+        song_2.play();
+        song_name_element.innerHTML = "The Current Playing Song Is 'Sorry'";
+        start()
+
+    } 
+    else if (score_rightWrist > 0.2 && !song_1.isPlaying()) {
+        song_2.stop();
+        song_1.play();
+        song_name_element.innerHTML = "The Current Playing Song Is 'DarkSide'";
+        start()
+    }
 }
 
 function play_1() {
@@ -56,7 +79,8 @@ function play_1() {
     song_2.stop();
     song_1.stop();
     song_2.play();
-    song_name_element.innerHTML += " Sorry";
+    song_name_element.innerHTML += " 'Sorry'";
+    start()
 }
 
 function play() {
@@ -64,5 +88,40 @@ function play() {
     song_1.stop();
     song_2.stop();
     song_1.play();
-    song_name_element.innerHTML += " DarkSide";
+    song_name_element.innerHTML += " 'DarkSide'";
+    start()
 }
+
+
+function start() {
+    recognition.start();
+
+}
+recognition.onresult = function run(event) {
+    console.log(event);
+
+    var Content = event.results[0][0].transcript;
+
+    console.log(Content);
+    if (Content == "Stop" || Content == "stop the song") {
+        speak()
+    }
+
+}
+function stopthesongs (){
+    song_1.stop();
+    song_2.stop();
+}
+
+
+function speak() {
+    synth = window.speechSynthesis;
+
+    speech = "Stopping the song";
+
+    utterThis = new SpeechSynthesisUtterance(speech);
+    synth.speak(utterThis);
+    setTimeout(function () {
+        stopthesongs ()
+         },5000);
+        }
